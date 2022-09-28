3e34f130905df26286de3d5956d6e3d89a263ed7
= <=>
2022-09-28 13:00:20 -0300

    Generated SpringBoot Application

---
+++ src/main/java/com/spotdle/SpotdleApplication.java
@@ -0,0 +1,13 @@
+package com.spotdle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotdleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotdleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotdle/SpotdleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotdle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotdleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
