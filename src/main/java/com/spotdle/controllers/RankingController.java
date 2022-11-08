c62b4bf1ab5504d8556dba788422e2830154985c
= <=>
2022-11-08 01:52:39 -0300

    A

--- src/main/java/com/spotdle/controllers/RankingController.java
+++ src/main/java/com/spotdle/controllers/RankingController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spotdle.services.SpotifyService;
@@ -30,6 +31,7 @@ import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.Image;
 import se.michaelthelin.spotify.model_objects.specification.Paging;
 
+@ResponseBody
 @RestController
 @RequestMapping("/me/user/ranking")
 public class RankingController {
