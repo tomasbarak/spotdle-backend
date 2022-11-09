7760e34a6aa1ddc401f50e5885b461088e23ba8e
tomasbarak <barakcinthia@gmail.com>
2022-11-09 11:32:11 -0300

    A

--- src/main/java/com/spotdle/controllers/UserController.java
+++ src/main/java/com/spotdle/controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spotdle.services.SpotifyService;
@@ -29,6 +30,7 @@ import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.Image;
 import se.michaelthelin.spotify.model_objects.specification.Paging;
 
+@ResponseBody
 @RestController
 @RequestMapping("/me/user")
 public class UserController {
